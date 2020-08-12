fc450178dec0bf064025415cd6c4087c5cca9785
Ashwin R <ashwinkey04@gmail.com>
2020-08-12 09:45:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# lorax
+
+Flutter app to help growing trees and keep track of them
+
---
+++ android/app/src/main/kotlin/org/amfoss/lorax/MainActivity.kt
@@ -0,0 +1,6 @@
+package org.amfoss.lorax
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
